7cf00a11bac011db91efe3b88c2bc44d9bbf676b
PeterBurrfot <arr.in.the.pw@gmail.com>
2023-10-30 23:57:02 +0300

    PinocchioAndApples

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int pinocchio = 10;
+        int karabas = 8;
+
+        int ostatokYablokYBuratino = pinocchio - karabas;
+
+        System.out.println("Количество яблок у Буратино: " + pinocchio);
+        System.out.println("Количество яблок украденные Карабасом-Барабасом: " + karabas);
+        System.out.println("Количество яблок осталось у Буратино: " + ostatokYablokYBuratino);
+    }
+}
